04b242a4def9395cb1452b0dae256a678ddb866b
abhinav bhateja <abhinav.abhi30@gmail.com>
2023-12-08 17:41:55 +0530

    adding concurrency logic for duplicate room request created at same time

--- src/main/java/com/vidyo/portal/service/RoomService.java
+++ src/main/java/com/vidyo/portal/service/RoomService.java
@@ -20,10 +20,16 @@ import org.springframework.ws.transport.context.TransportContextHolder;
 import org.springframework.ws.transport.http.HttpUrlConnection;
 
 import java.util.*;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class RoomService extends WebServiceGatewaySupport {
 
     private static final Logger log = LoggerFactory.getLogger(RoomService.class);
+
+    private final ConcurrentHashMap<String, ReentrantLock> roomLocks = new ConcurrentHashMap<>();
+
+    private final ConcurrentHashMap<String, RoomEntity> roomResponses = new ConcurrentHashMap<>();
 
     @Autowired
     private RoomConfig roomConfig;
@@ -31,7 +37,47 @@ public class RoomService extends WebServiceGatewaySupport {
     @Autowired
     private RoomRepository roomRepository;
 
-    public CreateScheduledRoomResponse createScheduleRoom(){
+    public RoomVO findOrCreateRoom(String roomName){
+        RoomEntity room = getRoomByName(roomName);
+        if(Objects.isNull(room)){
+            // Getting lock particular room request
+            ReentrantLock roomLock = getRoomLock(roomName);
+            roomLock.lock();
+            try{
+                // Checking again after obtaining the lock
+                room = roomResponses.get(roomName);
+                if(Objects.isNull(room)){
+                    // Creating room and saving it in the DB & internal storage
+                    CreateScheduledRoomResponse scheduledRoomResponse = createScheduleRoom();
+                    room = saveRoom(roomName, scheduledRoomResponse);
+                    roomResponses.put(roomName,room);
+                }
+            }finally {
+                synchronized (roomLock){
+                    // Releasing the lock and notifying the waiting threads for same room request
+                    roomLock.unlock();
+                    roomLock.notifyAll();
+                }
+            }
+        }
+
+        return RoomEntityToVOConverter.convertEntityToVO(room);
+    }
+
+    public List<RoomVO> fetchAllRooms(){
+        List<RoomEntity> roomsEntityList = roomRepository.findAllByDate(new Date());
+        if(CollectionUtils.isEmpty(roomsEntityList)){
+            return null;
+        }
+        return  RoomEntityToVOConverter.convertEntityListToVOList(roomsEntityList);
+    }
+
+    private RoomEntity getRoomByName(String roomName){
+        List<RoomEntity> list = roomRepository.findByName(roomName.toLowerCase());
+        return CollectionUtils.isEmpty(list)?null:list.get(0);
+    }
+
+    private CreateScheduledRoomResponse createScheduleRoom(){
         CreateScheduledRoomRequest req = new CreateScheduledRoomRequest();
 
         CreateScheduledRoomResponse response = (CreateScheduledRoomResponse) getWebServiceTemplate()
@@ -49,22 +95,7 @@ public class RoomService extends WebServiceGatewaySupport {
         return response;
     }
 
-    public RoomVO findOrCreateRoom(String roomName){
-        RoomEntity room = getRoomByName(roomName);
-        if(Objects.isNull(room)){
-            CreateScheduledRoomResponse scheduledRoomResponse = createScheduleRoom();
-            room = saveRoom(roomName, scheduledRoomResponse);
-        }
-
-        return RoomEntityToVOConverter.convertEntityToVO(room);
-    }
-
-    public RoomEntity getRoomByName(String roomName){
-        List<RoomEntity> list = roomRepository.findByName(roomName.toLowerCase());
-        return CollectionUtils.isEmpty(list)?null:list.get(0);
-    }
-
-    public RoomEntity saveRoom(String roomName, CreateScheduledRoomResponse scheduledRoomResponse){
+    private RoomEntity saveRoom(String roomName, CreateScheduledRoomResponse scheduledRoomResponse){
         RoomEntity entity = new RoomEntity();
         entity.setName(roomName.toLowerCase());
         entity.setRoomURL(scheduledRoomResponse.getRoomURL());
@@ -75,11 +106,21 @@ public class RoomService extends WebServiceGatewaySupport {
         return roomRepository.save(entity);
     }
 
-    public List<RoomVO> fetchAllRooms(){
-        List<RoomEntity> roomsEntityList = roomRepository.findAllByDate(new Date());
-        if(CollectionUtils.isEmpty(roomsEntityList)){
-            return null;
+    private ReentrantLock getRoomLock(String roomName){
+        // Get or create a lock for the specified roomName
+        ReentrantLock lock = roomLocks.computeIfAbsent(roomName, obj -> new ReentrantLock());
+
+        // Wait for the lock to be available if it's already locked
+        while(lock.isLocked() && !lock.isHeldByCurrentThread()){
+            try{
+                synchronized (lock) {
+                    lock.wait();
+                }
+            }catch (InterruptedException e){
+                Thread.currentThread().interrupt();
+            }
         }
-        return  RoomEntityToVOConverter.convertEntityListToVOList(roomsEntityList);
+        return lock;
     }
+
 }
